58076a4c6f9c8480013fca45830087a18dee5890
Lil Dumpling <davidruan7777@gmail.com>
2022-12-12 22:26:59 -0500

    Added "Page out of " text that works along with the buttons and switches out the schedules

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.finalproject
 
 import android.os.Bundle
-import android.util.DisplayMetrics
+//import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -37,16 +37,65 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(schedules: List<Schedule>) {
     Column(Modifier.fillMaxHeight()) {
+
         Row(Modifier.fillMaxWidth()) {
-            Text(text = "Your Schedules", modifier = Modifier.padding(all = 20.dp).padding(top=15.dp))
+            Text(text = "Your Schedules", modifier = Modifier
+                .padding(all = 20.dp)
+                .padding(top = 15.dp))
             Spacer(modifier = Modifier.width(140.dp))
             Button(onClick = {/*placeholder*/ }) {
                 Text(text = "edit ", modifier = Modifier.padding(all = 20.dp))
             }
         }
-        schedules.forEach{ scheduleName ->
-            Button(onClick = {/*placeholder*/ },modifier = Modifier.fillMaxWidth().padding(all=10.dp) ,colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow)) {
-                Text(text = scheduleName.firstProperty, Modifier.padding(all = 20.dp))
+
+        var schedNumStart by remember{
+            mutableStateOf(0)
+        }
+        var schedNumEnd by remember{
+            mutableStateOf(1)
+        }
+        for(i in schedNumStart..schedNumEnd){
+            Button(onClick = {/*placeholder*/ },modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 10.dp) ,colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow)) {
+                Text(text = schedules[i].firstProperty,
+                    Modifier
+                        .padding(all = 20.dp)
+                        .padding(top = 20.dp))
+            }
+        }
+//        schedules.forEach{ scheduleName ->
+//            Button(onClick = {/*placeholder*/ },modifier = Modifier.fillMaxWidth().padding(all=10.dp) ,colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow)) {
+//                Text(text = scheduleName.firstProperty, Modifier.padding(all = 20.dp).padding(top = 20.dp))
+//            }
+//        }
+        var pageNum by remember{
+            mutableStateOf(1)
+        }
+        val totalPageNum by remember{
+            mutableStateOf(7)
+        }
+
+        Row(Modifier.fillMaxWidth()) {
+            Text(text = "Page $pageNum out of $totalPageNum", modifier = Modifier
+                .padding(all = 20.dp)
+                .padding(top = 15.dp))
+
+        }
+        Row(Modifier.fillMaxWidth()) {
+            Button(onClick = {if(pageNum>1) {
+                pageNum--
+                schedNumEnd--
+                if(schedNumStart>0)schedNumStart--
+            }}) {
+                Text(text = "Previous ", modifier = Modifier.padding(all = 20.dp))
+            }
+            Button(onClick = {if(pageNum<totalPageNum) {
+                pageNum++
+                schedNumStart++
+                if(schedNumEnd<totalPageNum)schedNumEnd++
+            }}) {
+                Text(text = "Next ", modifier = Modifier.padding(all = 20.dp))
             }
         }
     }
