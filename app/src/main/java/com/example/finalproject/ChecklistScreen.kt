122743e1f1f0f85c22bd42897c6fdb719e0ff251
Nick <nickmcg07@gmail.com>
2023-01-09 19:38:59 -0500

    swap schedule view button

--- app/src/main/java/com/example/finalproject/ChecklistScreen.kt
+++ app/src/main/java/com/example/finalproject/ChecklistScreen.kt
@@ -27,65 +27,79 @@ fun ChecklistScreen(
 
     viewModel.findSchedule(scheduleID)
     val schedule: Schedule? = viewModel.searchResults.observeAsState().value?.get(0)
-    var checkedList = remember { mutableStateListOf<Boolean>()}
+    var checkedList = remember { mutableStateListOf<Boolean>() }
+    Column(Modifier.fillMaxHeight()) {
+        Column(Modifier.verticalScroll(rememberScrollState()).weight(1f)) {
 
-    Column(Modifier.fillMaxHeight().verticalScroll(rememberScrollState())) {
+            Row(Modifier.fillMaxWidth()) {
+                Text(
 
-        Row(Modifier.fillMaxWidth()) {
-            Text(
-
-                text = schedule?.name.toString(),
-                modifier = Modifier
-                    .padding(all = 20.dp)
-                    .padding(top = 15.dp)
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Button(onClick = {navController.navigate(route = Screens.Schedule.route)}, modifier = Modifier.width(150.dp)) {
-                Text(text = "exit", modifier = Modifier.padding(all = 20.dp))
-            }
-        }
-
-        var toSize: Int
-        if (schedule != null) {
-            toSize = schedule?.items?.scheduleItems?.size-1
-        } else {
-            toSize = 0
-        }
-
-        for (i in 0..toSize) {
-            if (i >= checkedList.size){
-                checkedList.add(false)
-            }
-            Button(
-                onClick = { checkedList[i] = !checkedList[i] },
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)
-                    .padding(vertical = 20.dp).height(100.dp)
-            ) {
-                if (checkedList[i]) {
-
-                    Image(
-                        painterResource(R.drawable.completedcheck),
-                        contentDescription = "Item Completed",
-                        modifier = Modifier.padding(horizontal = 5.dp)
-                            .align(Alignment.CenterVertically)
-                    )
-                } else {
-                    Image(
-                        painterResource(R.drawable.uncompletedcheck),
-                        contentDescription = "Item Not Completed",
-                        modifier = Modifier.padding(horizontal = 5.dp)
-                            .align(Alignment.CenterVertically)
-                    )
+                    text = schedule?.name.toString(),
+                    modifier = Modifier
+                        .padding(all = 20.dp)
+                        .padding(top = 15.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    onClick = { navController.navigate(route = Screens.Schedule.route) },
+                    modifier = Modifier.width(150.dp)
+                ) {
+                    Text(text = "exit", modifier = Modifier.padding(all = 20.dp))
                 }
+            }
+
+            if (schedule != null) {
+                for (i in 0..schedule?.items?.scheduleItems?.size - 1) {
+                    if (i >= checkedList.size) {
+                        checkedList.add(false)
+                    }
+                    Button(
+                        onClick = { checkedList[i] = !checkedList[i] },
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)
+                            .padding(vertical = 20.dp).height(100.dp)
+                    ) {
+                        if (checkedList[i]) {
+
+                            Image(
+                                painterResource(R.drawable.completedcheck),
+                                contentDescription = "Item Completed",
+                                modifier = Modifier.padding(horizontal = 5.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        } else {
+                            Image(
+                                painterResource(R.drawable.uncompletedcheck),
+                                contentDescription = "Item Not Completed",
+                                modifier = Modifier.padding(horizontal = 5.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
 //                Spacer(modifier = Modifier.weight(1f))
-                if (schedule != null) {
-                    Text(
-                        text = schedule.items.scheduleItems[i],
-                        modifier = Modifier.padding(all = 20.dp).weight(1f)
-                    )
+                        if (schedule != null) {
+                            Text(
+                                text = schedule.items.scheduleItems[i],
+                                modifier = Modifier.padding(all = 20.dp).weight(1f)
+                            )
+                        }
+                    }
                 }
             }
+        }
+        Column(Modifier.height(90.dp)) {
+            Row() {
+                Button(onClick = {/*To view 1*/ }, enabled = false) {
+                    Text(text = "Checklist")
+                }
+                Spacer(modifier = Modifier.weight(0.5f))
 
+                Button(onClick = {/*To view 2*/ }) {
+                    Text(text = "This Then")
+                }
+                Spacer(modifier = Modifier.weight(0.5f))
+                Button(onClick = {/*To view 3*/ }) {
+                    Text(text = "One At a Time")
+                }
+            }
         }
     }
 }
