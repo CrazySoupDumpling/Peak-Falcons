069f93b11e175798ce9e513a83538d7736d1d80a
CrazySoupDumpling <Davidruan7777@gmail.com>
2023-01-11 14:07:07 -0500

    fixed imaging and other spacing stuff to fix all the colors and everything else

--- app/src/main/java/com/example/finalproject/twoItemSched.kt
+++ app/src/main/java/com/example/finalproject/twoItemSched.kt
@@ -1,13 +1,10 @@
 package com.example.finalproject
 import androidx.navigation.compose.rememberNavController
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -20,8 +17,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import kotlin.collections.ArrayList
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+
+
+
+@Composable
+fun popUpCongrat(YesNo: Boolean){
+    if(YesNo){
+        Popup(
+            alignment = Alignment.TopCenter,
+            properties = PopupProperties()
+        ) {
+            Box(
+                Modifier
+                    .padding(top = 5.dp)
+
+                    .border(1.dp, color = Color.Black)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Text(
+                        text = "You have no more items to complete!",
+                        color = Color.White,
+                        modifier = Modifier.padding(vertical = 5.dp),
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 fun twoItemSched(
@@ -30,9 +63,11 @@ fun twoItemSched(
 
 ) {
 
+    var popUpYN by remember{ mutableStateOf(false) }
     val schedule = scheduleTesting
     var tIList = remember { mutableStateListOf<Boolean>()}
     var itemNum by remember { mutableStateOf(0) }
+
     Column(Modifier.fillMaxHeight(),
         verticalArrangement = Arrangement.SpaceBetween){
 
@@ -43,6 +78,8 @@ fun twoItemSched(
                 modifier = Modifier
                     .padding(all = 20.dp)
                     .padding(top = 15.dp)
+                    .align(Alignment.CenterVertically),
+                fontSize = 30.sp
             )
             Spacer(modifier = Modifier.weight(1f))
             Button(onClick = {navController.navigate(route = Screens.Schedule.route)}, modifier = Modifier.width(150.dp)) {
@@ -55,13 +92,26 @@ fun twoItemSched(
         } else {
             toSize = 0
         }
-        if(toSize == 0)navController.navigate(route = Screens.Schedule.route)
+        if(toSize == 0){
+            popUpYN = true
+            navController.navigate(route = Screens.Schedule.route)
+        }
+        if(popUpYN){
+            popUpCongrat(YesNo = true)
+        }
+        val imageModifier = Modifier
+            .size(150.dp)
+            .border(BorderStroke(0.dp, Color.Black))
+            //.background(Color.Yellow)
+            .align(Alignment.CenterHorizontally)
 
 
 
         Column(){
             Box(
-                modifier = Modifier.border(20.dp, Color.Black).fillMaxWidth()
+                modifier = Modifier
+                    .border(20.dp, Color.Black)
+                    .fillMaxWidth()
             ) {
                 Text(
                     text = schedule.items.scheduleItems[itemNum],
@@ -69,15 +119,18 @@ fun twoItemSched(
                     fontSize = 50.sp
                 )
             }
+            Spacer(modifier = Modifier.height(30.dp))
             if(itemNum+1<toSize) {
                 Image(
                     painterResource(R.drawable.downarrow),
                     contentDescription = "Item Completed",
-                    modifier = Modifier.padding(horizontal = 5.dp)
-                        .align(Alignment.CenterHorizontally)
+                    modifier = imageModifier
                 )
+                Spacer(modifier = Modifier.height(30.dp))
                 Box(
-                    modifier = Modifier.border(20.dp, Color.Black).fillMaxWidth()
+                    modifier = Modifier
+                        .border(20.dp, Color.Black)
+                        .fillMaxWidth()
                 ) {
                     Text(
                         text = schedule.items.scheduleItems[itemNum+1],
@@ -93,7 +146,8 @@ fun twoItemSched(
 
         Row(
             modifier = Modifier
-                .weight(1f, false).fillMaxWidth(),
+                .weight(1f, false)
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Button(
@@ -105,15 +159,15 @@ fun twoItemSched(
             //Spacer(modifier = Modifier.weight(.25f))
             Button(
                 onClick = {
-                    if(itemNum<toSize)itemNum++
-//                    if(itemNum==toSize){Popup(
-//                        alignment = Alignment.Center,
-//                    ){Text("YOU ARE FINISHED")}
-//                          }
-                          },
-                modifier = Modifier.width(200.dp)
-            ) {
-                Text(text = "Next", modifier = Modifier.padding(all = 40.dp), fontSize = 20.sp)
+                    if(itemNum<toSize)itemNum++;
+                    if(itemNum==toSize){
+                        popUpYN = true;
+                        navController.navigate(route = Screens.Schedule.route)}
+                },
+                    modifier = Modifier.width(200.dp)
+                    ) {
+
+                    Text(text = "Next", modifier = Modifier.padding(all = 40.dp), fontSize = 20.sp)
             }
         }
         //Spacer(modifier = Modifier.weight(.1f))
@@ -126,6 +180,7 @@ fun twoItemSched(
 @Composable
 fun twoPreview() {
     val list = SnapshotStateList<String>()
+    val list1 = SnapshotStateList<String>()
     val items = ScheduleItems(list)
     val schedTest = Schedule("Test1",items)
 
